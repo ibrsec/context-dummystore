7ca03f7d6693bbd35b9e7339b1decb045864099c
ibr <ibr.seckin@gmail.com>
2024-04-29 14:20:12 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Products from "./pages/Products";
+// import About from "./pages/About";
+// import NotFound from "./pages/NotFound";
+
+import AuthProvider from "./context/AuthProvider";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+      {/*//* route yapısını router dosyasına taşıdık */}
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter> */}
     </div>
+    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+    //   <span className="animate-bounce">🏀</span>
+    //   <h1 className="animate-pulse">
+    //     Welcome to Session 1 of the Clarus Store Project FS16!
+    //   </h1>
+    //   <span className="animate-bounce">🏀</span>
+    // </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        main:"#FEAF00",
+        navbarColor:"#f2eae1",
+        white:"#6c6c6c",
+        white:"#FFF",
+      },
+      fontFamily:{
+        montserrat:["Montserrat", "sans-serif"]
+      },
+      fontSize:{
+        label:["14px",{
+          lineHeight:"normal",
+          fontWeight:"500"
+        }]
+      },
+      boxShadow:{
+        login: "2px 5px 10px 0px rgba(0,0,0,0.1)"
+      }
+    },
+  },
+  plugins: [],
+}
+
